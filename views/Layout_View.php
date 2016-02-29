8127fdf8c7fbb25f300996f5cf41412019fe56f5
Raul Castro <Raul Castro>
2016-02-29 17:17:29 -0500

    the place video url placeholder

--- views/Layout_View.php
+++ views/Layout_View.php
@@ -1201,9 +1201,9 @@ class Layout_View
 					{
 						?>
 					<div class="form-group">
-						<label class="col-sm-2 control-label" for="textinput"><b>T&iacute;tulo tercera columna</b></label>
+						<label class="col-sm-2 control-label" for="textinput"><b>URL del Video</b></label>
 						<div class="col-sm-10">
-							<input type="text" placeholder="T&iacute;tulo tercera columna" class="form-control" id="singleVideo" value="<?php echo $section['video']; ?>">
+							<input type="text" placeholder="URL del Video" class="form-control" id="singleVideo" value="<?php echo $section['video']; ?>">
 						</div>
 					</div>
 						<?php
