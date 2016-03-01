42ccd5c9edb2ace3f8bdee8549fa7b8f408b68df
Raul Castro <Raul Castro>
2016-02-29 19:32:31 -0500

    some change on the directorio script, it was a fix

--- js/directorio.js
+++ js/directorio.js
@@ -15,56 +15,6 @@ $(document).ready(function()
 		return false;
 	});
 	
-	$(".upload-disr").uploadFile({
-		url:		"/ajax/media.php",
-		fileName:	"myfile",
-		multiple: 	true,
-		doneStr:	"uploaded!",
-		formData: {
-				opt: 6 
-			},
-		onSuccess:function(files, data, xhr)
-		{
-			obj 			= JSON.parse(data);
-			imageGallery 	= obj.fileName;
-			lastIdGallery 	= obj.lastId;
-			
-			itemGallery = '<div class="col-sm-12 slider-item" id="sId-'+lastIdGallery+'">'+
-			'	<div class="col-sm-12">'+
-			'		<div class="col-sm-4">'+
-			'			<img alt="" src="/images-system/medium/'+imageGallery+'" />'+
-			'		</div>'+
-			'		<div class="col-sm-offset-6 col-sm-2">'+
-			'			<a href="javascript:void(0);" class="btn btn-info btn-xs saveSlider" sId="'+lastIdGallery+'">Save</a>'+
-			'			<a href="javascript:void(0);" class="btn btn-danger btn-xs deleteSlider" sId="'+lastIdGallery+'">Delete</a>'+
-			'		</div>'+
-			'	</div>'+
-			'	<div class="col-sm-12 slider-section">'+
-			'		<div class="col-sm-4">'+
-			'			<input type="text" placeholder="Twitter" class="form-control" id="titleSlider-'+lastIdGallery+'" value="">'+
-			'		</div>'+
-			'		<div class="col-sm-4">'+
-			'			<input type="text" placeholder="Facebook" class="form-control" id="linkSlider-'+lastIdGallery+'" value="">'+
-			'		</div>'+
-			'		<div class="col-sm-4">'+
-			'			<input type="text" placeholder="Google Plus" class="form-control" id="gSlider-'+lastIdGallery+'" value="">'+
-			'		</div>'+
-			'	</div>'+
-			'</div>';
-			
-//			$('#slidersBox').prepend(itemGallery);
-//			
-//			$('.saveSlider').click(function(){
-//				saveSlider(this);
-//				return false;
-//			});
-//			
-//			$('.deleteSlider').click(function(){
-//				deleteSlider(this);
-//				return false;
-//			});
-		}
-	});
 });
 
 function addDirectorio()
@@ -106,13 +56,13 @@ function addDirectorio()
 					'	</div> ' +
 					'	<div class="col-sm-12 slider-section"> ' +
 					'		<div class="col-sm-4"> ' +
-					'			<input type="text" placeholder="Nombre" class="form-control" id="dirName-'+lastId+'" value=""> ' +
+					'			<input type="text" placeholder="Nombre" class="form-control" id="dirName-'+lastId+'" value="'+dirName+'"> ' +
 					'		</div> ' +
 					'		<div class="col-sm-4"> ' +
-					'			<input type="text" placeholder="Titulo" class="form-control" id="dirCharge-'+lastId+'" value=""> ' +
+					'			<input type="text" placeholder="Titulo" class="form-control" id="dirCharge-'+lastId+'" value="'+dirCharge+'"> ' +
 					'		</div> ' +
 					'		<div class="col-sm-4"> ' +
-					'			<input type="text" placeholder="E-Mail" class="form-control" id="dirEmail-'+lastId+'" value=""> ' +
+					'			<input type="text" placeholder="E-Mail" class="form-control" id="dirEmail-'+lastId+'" value="'+dirEmail+'"> ' +
 					'		</div> ' +
 					'	</div> ' +
 					'</div>';
